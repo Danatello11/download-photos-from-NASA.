7b4c83c4f496aa92ff67fd1b34d7708dfd2dbb10
Danatello11 <rai_dzio@mail.ru>
2024-07-01 02:48:13 +0500

    последний апдейт

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,28 +1,48 @@
 import os
 import random
+import time
+import schedule
 from telegram import Bot
+from dotenv import load_dotenv
 
-bot_token = "7185204155:AAFFrYXob7fs_iCkrr4XpWNgso82TywrYVI"
-group_chat_id = "-1002179626422"
+load_dotenv()
 
-def get_random_image(directory):
+# Получаем токен и ID группы из переменных окружения
+bot_token = os.getenv("BOT_TOKEN")
+group_chat_id = os.getenv("GROUP_CHAT_ID")
+publication_interval = int(os.getenv("PUBLICATION_INTERVAL", 4))  # задержка в часах по умолчанию
+
+directory = "downloaded_images"
+
+def get_random_images(directory):
     images = []
     for root, _, files in os.walk(directory):
         for file in files:
             if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
                 images.append(os.path.join(root, file))
-    if not images:
-        return None
-    return random.choice(images)
+    random.shuffle(images)
+    return images
 
-def send_random_image(bot_token, chat_id, image_path, caption=None):
+def send_image(bot_token, chat_id, image_path, caption=None):
     bot = Bot(token=bot_token)
     with open(image_path, 'rb') as image:
         bot.send_photo(chat_id=chat_id, photo=image, caption=caption)
 
+def publish_photos():
+    images = get_random_images(directory)
+    while True:
+        for image_path in images:
+            send_image(bot_token, group_chat_id, image_path, caption="Привет, привет")
+            time.sleep(publication_interval * 3600)  # переводим часы в секунды
+        random.shuffle(images)  # перемешиваем список после публикации всех фото
+
+
+def main():
+    schedule.every(publication_interval).hours.do(publish_photos)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
 if __name__ == "__main__":
-    image_path = get_random_image("downloaded_images")
-    if image_path:
-        send_random_image(bot_token, group_chat_id, image_path, caption="привет, это фото космоса")
-    else:
-        print("No images found in the specified directory.")
+    main()
