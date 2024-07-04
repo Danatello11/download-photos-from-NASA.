1e6d31e80073dc531478c67156a8bcb8e9ace251
Danatello11 <rai_dzio@mail.ru>
2024-07-05 01:20:01 +0500

    Update epic_earth_photos.py
    удалил лишние функции и упросил код
    Функция load_nasa_api_key() и get_epic_images_url(api_key) были удалены.
    Код для получения API-ключа и формирования URL для запроса изображений был интегрирован непосредственно в функцию save_earth_photos().
    В функции main(), API-ключ теперь извлекается напрямую из переменной окружения nasa_api с использованием os.getenv('nasa_api ').

--- epic_earth_photos.py
+++ epic_earth_photos.py
@@ -5,49 +5,41 @@ import urllib.parse
 from image_utils import download_image
 from image_utils import get_image_extension
 
-def load_nasa_api_key():
-    load_dotenv('.env')
-    return os.getenv('nasa_api_key')
-
-def get_epic_images_url(api_key):
-    base_url = "https://api.nasa.gov/EPIC/api/natural/images"
-    params = {
-        'api_key': api_key
-    }
-    return f"{base_url}?{urllib.parse.urlencode(params)}"
-
-
-def save_earth_photos(epic_images_data, num_photos, output_folder):
+def save_earth_photos(epic_images_data, num_photos, output_folder, api_key):
     if not epic_images_data:
         print("Не удалось получить данные о фотографиях Земли.")
         return
     
     os.makedirs(output_folder, exist_ok=True)
-    for index, image_info in enumerate(epic_images_data[:num_photos], start=1):
-        image_date = image_info["date"].split()[0].replace("-", "/")
-        image_name = image_info["image"]
-        image_url = f"https://epic.gsfc.nasa.gov/archive/natural/{image_date}/png/{image_name}.png"
-        extension = get_image_extension(image_url)
-        file_path = os.path.join(output_folder, f"earth_photo_{index}{extension}")
-        download_image(image_url, file_path)
-        print(f"Фотография Земли {index} сохранена: {file_path}")
-
-def get_epic_earth_photos(api_key, output_folder, num_photos=10):
-    url = get_epic_images_url(api_key)
+    base_url = "https://api.nasa.gov/EPIC/api/natural/images"
+    params = {
+        'api_key': api_key
+    }
+    url = f"{base_url}?{urllib.parse.urlencode(params)}"
+    
     try:
         response = requests.get(url)
         response.raise_for_status()
         epic_images_data = response.json()
-        save_earth_photos(epic_images_data, num_photos, output_folder)
+        
+        for index, image_info in enumerate(epic_images_data[:num_photos], start=1):
+            image_date = image_info["date"].split()[0].replace("-", "/")
+            image_name = image_info["image"]
+            image_url = f"https://epic.gsfc.nasa.gov/archive/natural/{image_date}/png/{image_name}.png"
+            extension = get_image_extension(image_url)
+            file_path = os.path.join(output_folder, f"earth_photo_{index}{extension}")
+            download_image(image_url, file_path)
+            print(f"Фотография Земли {index} сохранена: {file_path}")
+    
     except requests.exceptions.RequestException as e:
         print(f"Ошибка при получении данных: {e}")
 
 def main():
     load_dotenv('.env')
-    nasa_api_key = load_nasa_api_key()
+    nasa_api_key = os.getenv('nasa_api')
     output_folder = "downloaded_images/earth"
     num_photos = 10
-    get_epic_earth_photos(nasa_api_key, output_folder, num_photos)
+    save_earth_photos([], num_photos, output_folder, nasa_api_key)
 
 if __name__ == "__main__":
     main()
