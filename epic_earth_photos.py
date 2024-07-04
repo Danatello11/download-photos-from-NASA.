d1276f7f43bc0117629c8eb46fc36f8bef3cc4eb
Danatello11 <rai_dzio@mail.ru>
2024-07-05 01:41:11 +0500

    Update epic_earth_photos.py
    fetch_epic_images(api_key):
    
    Функция fetch_epic_images теперь отвечает только за получение данных о фотографиях Земли и возвращает их в виде списка. Логика проверки статуса ответа вынесена из save_earth_photos в эту функцию.
    save_earth_photos(epic_images, output_folder):
    
    Функция save_earth_photos теперь получает данные о фотографиях Земли через аргумент epic_images и сохраняет их в указанную папку.
    main():
    
    В main() сначала вызывается fetch_epic_images, чтобы получить данные, затем эти данные передаются в save_earth_photos.
    Таким образом, код стал более модульным и понятным, что улучшает его поддержку и расширяемость в будущем.

--- epic_earth_photos.py
+++ epic_earth_photos.py
@@ -5,12 +5,7 @@ import urllib.parse
 from image_utils import download_image
 from image_utils import get_image_extension
 
-def save_earth_photos(epic_images, num_photos, output_folder, api_key):
-    if not epic_images:
-        print("Не удалось получить данные о фотографиях Земли.")
-        return
-    
-    os.makedirs(output_folder, exist_ok=True)
+def fetch_epic_images(api_key):
     base_url = "https://api.nasa.gov/EPIC/api/natural/images"
     params = {
         'api_key': api_key
@@ -18,10 +13,20 @@ def save_earth_photos(epic_images, num_photos, output_folder, api_key):
     url = f"{base_url}?{urllib.parse.urlencode(params)}"
     
     response = requests.get(url)
-    response.raise_for_status()
-    epic_images = response.json()
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Ошибка при получении данных: {response.status_code}")
+        return []
+
+def save_earth_photos(epic_images, output_folder):
+    if not epic_images:
+        print("Не удалось получить данные о фотографиях Земли.")
+        return
     
-    for index, image_info in enumerate(epic_images[:num_photos], start=1):
+    os.makedirs(output_folder, exist_ok=True)
+    
+    for index, image_info in enumerate(epic_images, start=1):
         image_date = image_info["date"].split()[0].replace("-", "/")
         image_name = image_info["image"]
         image_url = f"https://epic.gsfc.nasa.gov/archive/natural/{image_date}/png/{image_name}.png"
@@ -35,7 +40,8 @@ def main():
     nasa_api_key = os.getenv('nasa_api')
     output_folder = "downloaded_images/earth"
     num_photos = 10
-    save_earth_photos([], num_photos, output_folder, nasa_api_key)
+    epic_images = fetch_epic_images(nasa_api_key)[:num_photos]
+    save_earth_photos(epic_images, output_folder)
 
 if __name__ == "__main__":
     main()
