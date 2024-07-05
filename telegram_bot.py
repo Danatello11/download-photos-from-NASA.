d4ab64acd828e3b6a2066dadd81e3067e77513a0
Danatello11 <rai_dzio@mail.ru>
2024-07-05 15:40:17 +0500

    Update telegram_bot.py
    Внутри функции не осталось глобальных переменных, кроме абсолютно необходимых и неизбежных.

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,6 +2,7 @@ import os
 import random
 import time
 import schedule
+from functools import partial
 from telegram import Bot
 from dotenv import load_dotenv
 
@@ -15,26 +16,28 @@ def get_random_images(directory):
     random.shuffle(images)
     return images
 
+
 def send_image(bot_token, chat_id, image_path, caption=None):
     bot = Bot(token=bot_token)
     with open(image_path, 'rb') as image:
         bot.send_photo(chat_id=chat_id, photo=image, caption=caption)
 
-def publish_photos(bot_token, group_chat_id, publication_interval, directory):
+
+def publish_photos(bot_token, group_chat_id, directory):
     images = get_random_images(directory)
-    while True:
-        for image_path in images:
-            send_image(bot_token, group_chat_id, image_path, caption="Привет, привет")
-            time.sleep(publication_interval * 3600)
-        random.shuffle(images)
+    for image_path in images:
+        send_image(bot_token, group_chat_id, image_path, caption="Привет, привет")
+        time.sleep(1)  # Вместо publication_interval уменьшим задержку до 1 секунды
+
 
 def main():
     load_dotenv()
     bot_token = os.getenv("TG_BOT_TOKEN")
     group_chat_id = os.getenv("TG_GROUP_CHAT_ID")
-    publication_interval = int(os.getenv("PUBLICATION_INTERVAL", 4))
     directory = "path_to_your_image_directory"
-    schedule.every(publication_interval).hours.do(publish_photos, bot_token, group_chat_id, publication_interval, directory)
+    publish_func = partial(publish_photos, bot_token, group_chat_id, directory) 
+    publication_interval = int(os.getenv("PUBLICATION_INTERVAL", 4))
+    schedule.every(publication_interval).hours.do(publish_func)
     while True:
         schedule.run_pending()
         time.sleep(1)
